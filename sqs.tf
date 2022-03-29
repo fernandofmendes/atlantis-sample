35184ef51d4542c1bc7984c42ca08c2f83ba8b59
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2022-03-29 15:46:20 -0300

    sqs

--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,5 @@
-resource "aws_sqs_queue" "terraform_queue" {
-  name                        = "teste-maycon.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-}
+# resource "aws_sqs_queue" "terraform_queue" {
+#   name                        = "teste-maycon.fifo"
+#   fifo_queue                  = true
+#   content_based_deduplication = true
+# }
