b2b8ea47c8a21b56e1895e89150c52a412127803
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2022-03-29 03:37:33 -0300

    atlantis

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
-# provider "aws" {
-#   region     = "us-east-1"
+provider "aws" {
+  region     = "us-east-1"
 #   access_key = "my-access-key"
 #   secret_key = "my-secret-key"
-# }
-
-provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::315719052208:role/atlantis"
-    session_name = "${var.atlantis_user}-${var.atlantis_repo_owner}-${var.atlantis_repo_name}-${var.atlantis_pull_num}"
-  }
 }
+
+# provider "aws" {
+#   assume_role {
+#     role_arn     = "arn:aws:iam::315719052208:role/atlantis"
+#     session_name = "${var.atlantis_user}-${var.atlantis_repo_owner}-${var.atlantis_repo_name}-${var.atlantis_pull_num}"
+#   }
+# }
