13db32314fe61404dd60422579118ff541b1de34
Fernando Mendes <fernando.mendes@ebury.com>
2023-02-13 00:30:11 -0300

    Teste

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket   = "syshero.com.br-tfstates"
-    key      = "tfstates/atlantis/atlantis-sample"
-    region   = "us-east-1"
-    # role_arn = "arn:aws:iam::315719052208:role/atlantis"
-    # can't use var.atlantis_user as the session name because
-    # interpolations are not allowed in backend configuration
-    # session_name = "${var.atlantis_user}" WON'T WORK
+  # backend "s3" {
+  #   bucket   = "syshero.com.br-tfstates"
+  #   key      = "tfstates/atlantis/atlantis-sample"
+  #   region   = "us-east-1"
+  #   # role_arn = "arn:aws:iam::315719052208:role/atlantis"
+  #   # can't use var.atlantis_user as the session name because
+  #   # interpolations are not allowed in backend configuration
+  #   # session_name = "${var.atlantis_user}" WON'T WORK
     
-  }
+  # }
 }
